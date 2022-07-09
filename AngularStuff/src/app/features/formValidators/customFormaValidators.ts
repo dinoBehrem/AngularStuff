46af7c4d6fe7561a3914d9b02c49d5d2cc12ded3
dinoBehrem <dinobehrem23@gmail.com>
2022-07-09 11:58:25 +0200

    Form group and form array custom validators

--- AngularStuff/src/app/features/formValidators/customFormaValidators.ts
+++ AngularStuff/src/app/features/formValidators/customFormaValidators.ts
@@ -1,18 +1,44 @@
-import { AbstractControl, ValidationErrors, FormGroup, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  ValidationErrors,
+  FormGroup,
+  ValidatorFn,
+  FormArray,
+} from '@angular/forms';
 
 // Reactive forms custom form control validator uses functions
-export function fcv_ValidateAge(control: AbstractControl): ValidationErrors | null{
-    if (control.value && control.value > 0) {
-        return null;
-    }
-    
-    return {'ageValid' : true};
+export function fcv_ValidateAge(
+  control: AbstractControl
+): ValidationErrors | null {
+  if (control.value && control.value > 0) {
+    return null;
+  }
+
+  return { ageValid: true };
 }
 
-// export function fgv_ValidatePattern(formGroup: FormGroup) : ValidationErrors | null {
-//     if (formGroup.get('name')?.value != formGroup.get('surname')?.value) {
-//         return null;
-//     }
+// Rective forms custom form group validator
+export const fgv_ValidatePattern: ValidatorFn = (
+  formGroup: AbstractControl
+): ValidationErrors | null => {
+  const name = formGroup.get('name')?.value;
+  const surname = formGroup.get('surname')?.value;
+  if (name == surname) {
+    return { patternMatch: true };
+  }
 
-//     return { 'patterMatch' : true};
-// }
+  return null;
+};
+
+// Rective forms custom form array validator
+export const fav_CheckLength: ValidatorFn = (
+  formArray: AbstractControl
+): ValidationErrors | null => {
+  const length = (formArray as FormArray).length;
+
+  if (length > 0) {
+    return null;
+  }
+
+  return { isEmpty: true };
+};
--- AngularStuff/src/app/features/forms/reactive-forms/reactive-forms.component.ts
+++ AngularStuff/src/app/features/forms/reactive-forms/reactive-forms.component.ts
@@ -1,59 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormArray,
+} from '@angular/forms';
 import { User, Job, Task } from './user';
-import { fcv_ValidateAge } from '../../formValidators/customFormaValidators';
+import {
+  fav_CheckLength,
+  fcv_ValidateAge,
+  fgv_ValidatePattern,
+} from '../../formValidators/customFormaValidators';
 
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
-  styleUrls: ['./reactive-forms.component.scss']
+  styleUrls: ['./reactive-forms.component.scss'],
 })
 export class ReactiveFormsComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  user : FormGroup = new FormGroup({
-    name : new FormControl('Dino'),
-    surname : new FormControl('Behrem'),
-    age : new FormControl(30, [fcv_ValidateAge]),
-    isActive : new FormControl(true),
-    job: this.formBuilder.group({
-      title: ["", [Validators.required, Validators.minLength(4)]],
-      description: ["", [Validators.minLength(15)]]
-    }),
-    tasks: this.formBuilder.array([], [Validators.required])
-  },);
+  user: FormGroup = new FormGroup(
+    {
+      name: new FormControl('Dino'),
+      surname: new FormControl('Behrem'),
+      // updateOn blur metohod check for errors  upon submit
+      age: new FormControl(30, {
+        validators: [fcv_ValidateAge],
+        updateOn: 'blur',
+      }),
+      isActive: new FormControl(true),
+      job: this.formBuilder.group({
+        title: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(4)],
+        }),
+        description: new FormControl('', [Validators.minLength(15)]),
+      }),
+      tasks: this.formBuilder.array([], [fav_CheckLength]),
+    },
+    [fgv_ValidatePattern]
+  );
 
-  korisnik! : User;
-  constructor(private formBuilder: FormBuilder) { }
+  korisnik!: User;
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  printValues(){
-    
+  printValues() {
     console.log(this.user.get('age')?.errors);
 
     this.korisnik = this.user.value;
 
     this.korisnik.job = this.user.value.job as Job;
     this.korisnik.tasks = this.user.value.tasks as Task[];
-    
+
     console.log(this.korisnik);
-    console.log(this.korisnik?.job?.title || "Job not initialized");
-    console.log(this.korisnik?.tasks || "Tasks not initialized");
+    console.log(this.korisnik?.job?.title || 'Job not initialized');
+    console.log(this.korisnik?.tasks || 'Tasks not initialized');
   }
 
   getTasks(): FormArray {
-    return this.user.get("tasks") as FormArray;
+    return this.user.get('tasks') as FormArray;
   }
 
-  newTask() : FormGroup {
+  newTask(): FormGroup {
     return this.formBuilder.group({
-      name: ["", [Validators.required]],
-      status: false
+      name: ['', [Validators.required]],
+      status: false,
     });
   }
 
-  addTask(){
+  addTask() {
     this.getTasks().push(this.newTask());
   }
 }
